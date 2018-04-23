a62b2444695c777a90fc225ad776e137fdba4388
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-04-23 09:46:22 -0400

    Changing up the layout to allow for math. Also, added math.

--- js/calculator-function.js
+++ js/calculator-function.js
@@ -1,23 +1,48 @@
 window.onload = function() {
+    //I thought about putting the screen here, but it changes too often. Using Let statements to keep memory good.
+    var numButtons = document.getElementsByClassName("num-button");
+    var equals = document.getElementById("equals");
+    var del = document.getElementById("del");
+    var clear = document.getElementById("clear");
     
-    var elements = document.getElementsByTagName("button");
-    
-    for(var i = 0; i<elements.length; i++){
-        elements[i].addEventListener("click", function() {
-            var screen = document.getElementsByClassName("screen")[0].innerHTML;
-            if(screen === "0") {
+    //Even Listeners for our numbers/main operators that can run in the eval()
+    for(var i = 0; i<numButtons.length-1; i++){
+        numButtons[i].addEventListener("click", function() {
+            let screenFirst = document.getElementsByClassName("screen")[0].innerHTML;
+            //If the first item on the screen is a 0, we remove it
+            if(screenFirst === "0") {
                 document.getElementsByClassName("screen")[0].innerHTML = this.value;
-            } else {
+                //otherwise, we concat to the actual screen
+            } else if (screenFirst !== "0") {
                 document.getElementsByClassName("screen")[0].innerHTML += this.value;
-            }
+                console.log(this.value);
+            }    
         })
     }
+    
+    //Doing the math on the equals button
+    equals.addEventListener("click", function() {
+        try {
+            let screen = document.getElementsByClassName("screen")[0];    
+            screen.innerHTML = eval(screen.innerHTML);
+        }
+        catch(e) {
+            document.getElementsByClassName("screen")[0].innerHTML = "Math Error";
+        }
+    })
+    
+    clear.addEventListener("click", function(){
+        let screen = document.getElementsByClassName("screen")[0];
+        screen.innerHTML = "0";
+    })
+    del.addEventListener("click", function(){
+        let screen = document.getElementsByClassName("screen")[0];
+        screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length-1);
+    })
 };
 //
 //Next Steps:
 //
-//    Actual Math
 //    fix weird screen widening with large numbers
-//    Add a clear button 
 //    Style this to look more like an actual calculator
 //    style this to be more pretty
